9a68918d0296dd78e674ed8eb967554b099f5678
LynxSaiko <redteamsectorid@gmail.com>
2025-11-22 09:38:17 +0700

    first commit

---
+++ README.md
@@ -0,0 +1,5 @@
+# lazy1
+# lazy1
+# lazy1
+# lazy1
+# lazy1
---
+++ push.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+git init
+git add .
+git commit -m "first commit"
+git branch -M main
+git remote add origin https://github.com/LynxSaiko/LazyFrameworkGUI.git
+
+git push -u origin main
